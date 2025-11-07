9f13b13f258991e69e9089d93ec40ba62500acef
yg2505 <ygoyal2505@gmail.com>
2025-11-07 13:53:24 +0530

    deployment

--- nourishme-frontend/src/services/authService.jsx
+++ nourishme-frontend/src/services/authService.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/auth";
+const API_URL = "https://nourishme.onrender.com/api";
 
 export const signup = async (name, email, password) => {
-  const res = await axios.post(`${API_URL}/signup`, { name, email, password });
+  const res = await axios.post(`${API_URL}/auth/signup`, { name, email, password });
   return res.data;
 };
 
 export const login = async (email, password) => {
-  const res = await axios.post(`${API_URL}/login`, { email, password });
+  const res = await axios.post(`${API_URL}/auth/login`, { email, password });
   return res.data;
 };
